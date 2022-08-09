7be42d916de25feacb492bba5cb409e9d0d7f0f4
Juan Lutri <programando.juanlutri@gmail.com>
2022-08-08 23:39:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let titulo = document.getElementById("titulo");
+titulo.style.textTransform="uppercase";
+
+let paragraph = document.getElementsByTagName("p");
+for(let i = 0 ;i<paragraph.length ; i++);{
+    paragraph[i].style.color = "#ff4f4f";
+    
+}
